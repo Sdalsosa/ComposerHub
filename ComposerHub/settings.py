00438a699df4e8e8d758c02ad9f143ed77468710
Al <eganalan@gmail.com>
2023-01-17 02:27:37 +0000

    Add users app and configure
    Add:
    - users app
    - urls.py to users app
    - templates to users app
    - user-profile.html page
    
    Update:
    - settings.py with users app
    - views and urls for users app

--- ComposerHub/settings.py
+++ ComposerHub/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'compositions.apps.CompositionsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
