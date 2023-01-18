8e8e990421687d3fd74ae25530ab23bc93035b19
Al <eganalan@gmail.com>
2023-01-18 22:50:03 +0000

    Add ElephantSQL Database connection and migrate

--- ComposerHub/settings.py
+++ ComposerHub/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 from decouple import config
 from dotenv import load_dotenv
 import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 load_dotenv()
 
@@ -25,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,13 +84,16 @@ WSGI_APPLICATION = 'ComposerHub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
