c638b351b6d15dfe8ec73e8d0f598c2eaec5a5b8
Al <eganalan@gmail.com>
2023-01-19 16:15:13 +0000

    Add content and styling to home page

--- users/views.py
+++ users/views.py
@@ -7,3 +7,7 @@ def profiles(request):
     profiles = Profile.objects.all()
 
     return render(request, 'users/profiles.html', {'profiles': profiles})
+
+def profile(request, prim_key):
+    profile= Profile.objects.get(id=prim_key)
+    return render(request, 'users/user-profile.html', {'profile': profile})
